b84abf85b0442ffa3c41c7364a6437afa3d8bb57
Array_fish <uekusa_hideaki@yahoo.co.jp>
2023-09-11 01:34:37 +0900

    introduce Vue3

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,70 @@ mod utils;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[derive(Clone, Debug)]
+pub struct Todo {
+    id: u32,
+    title: String,
+    completed: bool,
+}
+
+impl Todo{
+    pub fn new(id: u32, title: String, completed: bool) -> Self {
+        Self {
+            id,
+            title,
+            completed,
+        }
+    }
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-todo!");
+impl Todo{
+    pub fn id(&self) -> u32 {
+        self.id
+    }
+
+    pub fn title(&self) -> String {
+        self.title.clone()
+    }
+
+    pub fn completed(&self) -> bool {
+        self.completed
+    }
+
+    pub fn set_completed(&mut self, completed: bool) {
+        self.completed = completed;
+    }
+}
+
+#[wasm_bindgen]
+pub struct TodoList {
+    todos: Vec<Todo>,
+}
+
+#[wasm_bindgen]
+impl TodoList{
+    pub fn new() -> Self {
+        Self {
+            todos: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, title: String) {
+        let id = self.todos.len() as u32;
+        let todo = Todo::new(id,title, false);
+        self.todos.push(todo);
+    }
+
+    pub fn get(&self, id: u32) -> Option<Todo> {
+        self.todos.get(id as usize).cloned()
+    }
+
+    pub fn remove(&mut self, id: u32) {
+        self.todos.remove(id as usize);
+    }
+
+    pub fn todos(&self) -> *const Todo {
+        self.todos.as_ptr()
+    }
 }
---
+++ vue-project/src/components/wasmTodo.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+
+import {TodoList} from '../../../pkg/wasm_todo'
+
+const todoList = TodoList.new()
+todoList.add('Hello from Vue!')
+todoList.add('Hello from Rust!')
+todoList.add('Hello from WebAssembly!')
+
+console.log(todoList.todos())
+</script>
+
+<template>
+  <ul>
+    <li v-for="todo in todoList.todos" :key="todo.id">
+      {{ todo.title }}
+    </li>
+  </ul>  
+</template>
